ffb90c2845df1e9fd9570690624ea51e3ae89e8a
bichotll <jaume@bic.cat>
2015-01-14 22:09:30 +0100

    first boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name androidLineupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the androidLineupApp
+ */
+angular.module('androidLineupApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name androidLineupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the androidLineupApp
+ */
+angular.module('androidLineupApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
